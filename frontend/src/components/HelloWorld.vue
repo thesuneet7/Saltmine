a8367758f5e1a241583e4f477762c8feec49bd21
Suneet Maharana <suneetyoyo7@gmail.com>
2025-03-07 02:50:35 +0530

    .

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -2,76 +2,21 @@
   <div id="particles-js"></div>
   <div class="container">
     <h1 class="gradient-text">Grid Generator</h1>
-    <div class="tabs">
-      <button :class="{ active: activeTab === 'periphery' }" @click="setActiveTab('periphery')">Periphery</button>
-      <button :class="{ active: activeTab === 'diagonal' }" @click="setActiveTab('diagonal')">Diagonal</button>
-      <button :class="{ active: activeTab === 'adj_const' }" @click="setActiveTab('adj_const')">Adjacency</button>
-      <button :class="{ active: activeTab === 'no_adj_const' }" @click="setActiveTab('no_adj_const')">No Adjacency</button>
-    </div>
-
-    <div class="form-container" v-if="activeTab === 'periphery'">
-      <label>Row:</label>
-      <input type="number" v-model="peripheryForm.n" />
-      <label>Column:</label>
-      <input type="number" v-model="peripheryForm.m" />
-      <label>Red:</label>
-      <input type="number" v-model="peripheryForm.red" />
-      <label>Green:</label>
-      <input type="number" v-model="peripheryForm.green" />
-      <label>Blue:</label>
-      <input type="number" v-model="peripheryForm.blue" />
-      <label>Periphery Colors (e.g., RGB):</label>
-      <input type="text" v-model="peripheryForm.peripheryColorsInput" />
-      <button class="generate-btn" @click="generatePeripheryGrid">Generate Grid</button>
-    </div>
-
-    <div class="form-container" v-if="activeTab === 'diagonal'">
-      <label>Dimension:</label>
-      <input type="number" v-model="diagonalForm.dimension" />
-      <label>Red:</label>
-      <input type="number" v-model="diagonalForm.red" />
-      <label>Green:</label>
-      <input type="number" v-model="diagonalForm.green" />
-      <label>Blue:</label>
-      <input type="number" v-model="diagonalForm.blue" />
-      <label>Diagonal Colors (e.g., RGB):</label>
-      <input type="text" v-model="diagonalForm.diagonalColorsInput" />
-      <button class="generate-btn" @click="generateDiagonalGrid">Generate Grid</button>
-    </div>
-
-    <div class="form-container" v-if="activeTab === 'adj_const'">
-      <label>Row:</label>
-      <input type="number" v-model="adjForm.n" />
-      <label>Column:</label>
-      <input type="number" v-model="adjForm.m" />
-      <label>Red:</label>
-      <input type="number" v-model="adjForm.red" />
-      <label>Green:</label>
-      <input type="number" v-model="adjForm.green" />
-      <label>Blue:</label>
-      <input type="number" v-model="adjForm.blue" />
-      <label>Adjacent Colours (e.g., RGB):</label>
-      <input type="text" v-model="adjForm.adjColorsInput" />
-      <button class="generate-btn" @click="generateAdjacentGrid">Generate Grid</button>
-    </div>
-
-    <div class="form-container" v-if="activeTab === 'no_adj_const'">
-      <label>Row:</label>
-      <input type="number" v-model="no_adjForm.n" />
-      <label>Column:</label>
-      <input type="number" v-model="no_adjForm.m" />
-      <label>Red:</label>
-      <input type="number" v-model="no_adjForm.red" />
-      <label>Green:</label>
-      <input type="number" v-model="no_adjForm.green" />
-      <label>Blue:</label>
-      <input type="number" v-model="no_adjForm.blue" />
-      <button class="generate-btn" @click="generateNoAdjacentGrid">Generate Grid</button>
-    </div>
-
     
+    <div class="tabs">
+      <button v-for="tab in tabs" :key="tab.id" :class="{ active: activeTab === tab.id }" @click="setActiveTab(tab.id)">{{ tab.label }}</button>
+    </div>
 
-    <div v-if="grid.length > 0" class="grid-container">
+    <div class="form-container" v-if="activeTab">
+      <template v-for="(value, key) in formValues[activeTab]" :key="key">
+        <label>{{ key.replace(/([A-Z])/g, ' $1') }}:</label>
+        <input v-if="key.includes('ColorsInput')" type="text" v-model="formValues[activeTab][key]" />
+        <input v-else type="number" v-model="formValues[activeTab][key]" />
+      </template>
+      <button class="generate-btn" @click="generateGrid">Generate Grid</button>
+    </div>
+    
+    <div v-if="grid.length" class="grid-container">
       <table>
         <tr v-for="(row, rowIndex) in grid" :key="rowIndex">
           <td v-for="(cell, cellIndex) in row" :key="cellIndex" :style="getCellStyle(cell)"></td>
@@ -88,44 +33,21 @@
 <script>
 import axios from 'axios';
 
-
-
 export default {
   data() {
     return {
       activeTab: 'periphery',
-      peripheryForm: {
-        n: 5,
-        m: 5,
-        red: 5,
-        green: 5,
-        blue: 5,
-        peripheryColorsInput: 'RGB',
-      },
-
-      diagonalForm: {
-        dimension: 5,
-        red: 5,
-        green: 5,
-        blue: 5,
-        diagonalColorsInput: 'RGB',
-      },
-
-      adjForm: {
-        n: 5,
-        m: 5,
-        red: 5,
-        green: 5,
-        blue: 5,
-        adjColorsInput: 'RGB',
-      },
-
-      no_adjForm: {
-        n: 5,
-        m: 5,
-        red: 5,
-        green: 5,
-        blue: 5,
+      tabs: [
+        { id: 'periphery', label: 'Periphery' },
+        { id: 'diagonal', label: 'Diagonal' },
+        { id: 'adj_const', label: 'Adjacency' },
+        { id: 'no_adj_const', label: 'No Adjacency' }
+      ],
+      formValues: {
+        periphery: { n: 5, m: 5, red: 5, green: 5, blue: 5, peripheryColorsInput: 'RGB' },
+        diagonal: { dimension: 5, red: 5, green: 5, blue: 5, diagonalColorsInput: 'RGB' },
+        adj_const: { n: 5, m: 5, red: 5, green: 5, blue: 5, adjColorsInput: 'RGB' },
+        no_adj_const: { n: 5, m: 5, red: 5, green: 5, blue: 5 }
       },
       grid: [],
       error: null,
@@ -137,118 +59,20 @@ export default {
       this.grid = [];
       this.error = null;
     },
-    generatePeripheryGrid() {
-      const payload = {
-        n: this.peripheryForm.n,
-        m: this.peripheryForm.m,
-        red: this.peripheryForm.red,
-        green: this.peripheryForm.green,
-        blue: this.peripheryForm.blue,
-        constraint_type: 'periphery',
-        periphery_colors: this.peripheryForm.peripheryColorsInput.split(''),  // Ensure it's split correctly
-      };
+    generateGrid() {
+      const form = this.formValues[this.activeTab];
+      const payload = { ...form, constraint_type: this.activeTab };
+      if (Object.prototype.hasOwnProperty.call(form, 'peripheryColorsInput')) 
+        payload.periphery_colors = form.peripheryColorsInput.split('');
+      if (Object.prototype.hasOwnProperty.call(form, 'diagonalColorsInput')) 
+        payload.diagonal_colors = form.diagonalColorsInput.split('');
+      if (Object.prototype.hasOwnProperty.call(form, 'adjColorsInput')) 
+        payload.adjacency_cons = form.adjColorsInput.split('');
+
+
       
       console.log("Sending Payload:", payload);
-      axios
-        .post('http://127.0.0.1:8000/generate-grid', {
-          n: this.peripheryForm.n,
-          m: this.peripheryForm.m,
-          red: this.peripheryForm.red,
-          green: this.peripheryForm.green,
-          blue: this.peripheryForm.blue,
-          constraint_type: 'periphery',
-          periphery_colors: this.peripheryForm.peripheryColorsInput.split(''),
-        })
-        .then(response => {
-          this.grid = response.data.grid;
-          this.error = null;
-        })
-        .catch(error => {
-          this.grid = [];
-          this.error = error.response?.data?.error || 'An error occurred.';
-        });
-    },
-    generateDiagonalGrid() {
-      const payload = {
-        dimension: this.diagonalForm.dimension,
-        red: this.diagonalForm.red,
-        green: this.diagonalForm.green,
-        blue: this.diagonalForm.blue,
-        constraint_type: 'diagonal',
-        diagonal_colors: this.diagonalForm.diagonalColorsInput.split(''), // Fix split
-  };
-      
-      console.log("Sending Payload:", payload);
-      axios
-        .post('http://127.0.0.1:8000/generate-grid', {
-          dimension: this.diagonalForm.dimension,
-          red: this.diagonalForm.red,
-          green: this.diagonalForm.green,
-          blue: this.diagonalForm.blue,
-          constraint_type: 'diagonal',
-          diagonal_colors: this.diagonalForm.diagonalColorsInput.split(''),
-        })
-        .then(response => {
-          this.grid = response.data.grid;
-          this.error = null;
-        })
-        .catch(error => {
-          this.grid = [];
-          this.error = error.response?.data?.error || 'An error occurred.';
-        });
-    },
-    generateAdjacentGrid() {
-      console.log("Adjacency Colors Input:", this.adjForm.adjColorsInput);
-      const payload = {
-        n: this.adjForm.n,
-        m: this.adjForm.m,
-        red: this.adjForm.red,
-        green: this.adjForm.green,
-        blue: this.adjForm.blue,
-        constraint_type: 'adj_const',
-        adjacency_cons: this.adjForm.adjColorsInput.split(''), // Fix split
-  };
-      
-      console.log("Sending Payload:", payload);
-      axios
-        .post('http://127.0.0.1:8000/generate-grid', {
-          n: this.adjForm.n,
-          m: this.adjForm.m,
-          red: this.adjForm.red,
-          green: this.adjForm.green,
-          blue: this.adjForm.blue,
-          constraint_type: 'adj_const',
-          adjacency_cons: this.adjForm.adjColorsInput.split(''),
-        })
-        .then(response => {
-          this.grid = response.data.grid;
-          this.error = null;
-        })
-        .catch(error => {
-          this.grid = [];
-          this.error = error.response?.data?.error || 'An error occurred.';
-        });
-    },
-    generateNoAdjacentGrid() {
-      const payload = {
-        n: this.no_adjForm.n,
-        m: this.no_adjForm.m,
-        red: this.no_adjForm.red,
-        green: this.no_adjForm.green,
-        blue: this.no_adjForm.blue,
-        constraint_type: 'no_adj_const',
-  };
-      
-      console.log("Sending Payload:", payload);
-      axios
-        .post('http://127.0.0.1:8000/generate-grid', {
-          n: this.no_adjForm.n,
-          m: this.no_adjForm.m,
-          red: this.no_adjForm.red,
-          green: this.no_adjForm.green,
-          blue: this.no_adjForm.blue,
-          constraint_type: 'no_adj_const',
-        })
+      axios.post('http://127.0.0.1:8000/generate-grid', payload)
         .then(response => {
           this.grid = response.data.grid;
           this.error = null;
@@ -264,6 +88,7 @@ export default {
   },
 };
 </script>
+
 
 <style scoped>
 
