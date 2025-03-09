d9a8e4c932c173dcc0b5903672da01925080e4fa
Suneet Maharana <suneetyoyo7@gmail.com>
2025-03-09 17:07:07 +0530

    .

--- frontend/src/components/GridGenerator.vue
+++ frontend/src/components/GridGenerator.vue
@@ -1,8 +1,10 @@
 <template>
     <div id="particles-js"></div>
     <div class="container">
-      <h1 class="gradient-text">Grid Generator</h1>
-      <button class="navigate-btn" @click="navigateToHelloWorld">Choose Constraints</button>
+      <h1 class="gradient-text fixed-width">Grid Generator</h1>
+      <h2 class="subheading">Simplify your Grid creation</h2>
+      <p class="description">An easy-to-use tool for generating customizable grids with constraints.</p>
+      <button class="navigate-btn" @click="navigateToHelloWorld">Start</button>
     </div>
   </template>
   
@@ -30,28 +32,57 @@
   }
   
   .container {
-    max-width: 600px;
-    margin: auto;
-    font-family: "Poppins", sans-serif;
-    text-align: center;
-    padding-top: 50px;
-  }
+  max-width: 700px;
+  margin: auto;
+  font-family: "Poppins", sans-serif;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  min-height: 60vh; /* Ensures full viewport height */
+  padding-top: 0; /* Remove unnecessary top padding */
+}
   
   .gradient-text {
-    font-size: 2.5rem;
+    font-size: 5rem;
     font-weight: 700;
+    text-align: center;
+    width: 100%;
     background: linear-gradient(90deg, #1e90ff, #ff6347);
     -webkit-background-clip: text;
     -webkit-text-fill-color: transparent;
+    margin-bottom: 10px;
   }
-  
+
+  .subheading {
+  font-size: 3rem;
+  font-weight: 600;
+  color: #1e1e1e;
+  margin-top: 5px;
+  margin-bottom: 5px;
+  }
+
+  .description {
+  font-size: 1.2rem;
+  font-weight: 200;
+  color: #6e6e6e;
+  max-width: 600px;
+  margin-top: 3px;
+  margin-bottom: 20px;
+  }
+
+  .fixed-width {
+    max-width: 100%;
+    white-space: nowrap;
+  }
   .navigate-btn {
     background-color: #ffffff;
     color: rgb(192, 120, 120);
     font-weight: bold;
     border: none;
     padding: 12px 24px;
-    font-size: 16px;
+    font-size: 18px;
     border-radius: 12px;
     position: relative;
     overflow: hidden;
